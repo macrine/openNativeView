d3d18794ab1e40120ca5efbd94ef11f1cf5c6fe7
mac <mac@qq.com>
2020-04-30 14:10:05 +0800

    修改

--- src/android/openNativeView/Gate.java
+++ src/android/openNativeView/Gate.java
@@ -2,13 +2,9 @@ package com.doves.plugins.openNativeView;
 
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONException;
-
-
 
 public class Gate extends CordovaPlugin {
 
@@ -20,11 +16,8 @@ public class Gate extends CordovaPlugin {
     return false;  // Returning false results in a "MethodNotFound" error.
   }
 
-
-
   private void openActivity(org.json.JSONArray args, final CallbackContext callbackcontext) {
     try {
-
       Context myContext = cordova.getActivity();
       String androidClass = args.getString(0);
       Intent intent = new Intent(myContext, Class.forName(androidClass));
@@ -32,14 +25,11 @@ public class Gate extends CordovaPlugin {
       intent.putExtra("params2",args.getString(3));
       intent.putExtra("params3",args.getString(4));
       intent.putExtra("params4",args.getString(5));
-      intent.putExtra("params5",args.getString(6));
-
       myContext.startActivity(intent);
-;
-      callbackcontext.success("取消发送成功");
+
+      callbackcontext.success("成功");
     } catch (Exception e) {
-      callbackcontext.error("发送失败");
-      Log.e("TTGH", "send message fail , e=" + e.getMessage());
+      callbackcontext.error("失败");
     }
   }
 
